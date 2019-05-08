2bc1dc8686301e0c01cd3c643f08576a9a6b2f91
Majid <mohammadmajid55@gmail.com>
2019-05-08 21:22:44 +0000

    username and messages can update and show on all clients

--- chatty_server/chatty-server.js
+++ chatty_server/chatty-server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const SocketServer = require('ws').Server;
+const UUID = require('uuid');
+const WebSocket = require ('ws');
 
 // Set the port to 3001
 const PORT = 3001;
@@ -13,19 +15,25 @@ const server = express()
 // Create the WebSockets server
 const wss = new SocketServer({ server });
 
-
-
-
 // Set up a callback that will run when a client connects to the server
 // When a client connects they are assigned a socket, represented by
 // the ws parameter in the callback.
 wss.on('connection', (ws) => {
   console.log('Client connected');
 
-  ws.on('message', (newText) => {
+    ws.on('message', (newText) => {
     let parsedMsg = JSON.parse(newText)
+    parsedMsg.id = UUID;
     console.log("User",parsedMsg.username,"said",parsedMsg.content);
-    // ws.send(parsedMsg)
+    console.log("wss client LENGTH", wss.clients.length);
+    
+    wss.clients.forEach(function  (client){
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(JSON.stringify(parsedMsg));
+        console.log("data", parsedMsg);
+      }
+    });
+  
   })
 
   // Set up a callback for when a client closes the socket. This usually means they closed their browser.
--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import ChatBar from './ChatBar.jsx';
 import MessageList from './MessageList.jsx';
 
@@ -8,21 +8,10 @@ class App extends Component {
     super(props);
     // this is the *only* time you should assign directly to state:
     this.state = {
-      currentUser: {name: "Bob"}, // optional. if currentUser is not defined, it means the user is Anonymous
-      messages: [
-        {
-          username: "Bob",
-          content: "Has anyone seen my marbles?",
-          id: 1
-        },
-        {
-          username: "Anonymous",
-          content: "No, I think you lost them. You lost your marbles Bob. You lost them for good.",
-          id: 2
-        }
-      ]
+      currentUser: { name: "Bob" }, // optional. if currentUser is not defined, it means the user is Anonymous
+      messages: []
     };
-    
+  
     this.socket = new WebSocket('ws://localhost:3001');
 
   }
@@ -30,35 +19,29 @@ class App extends Component {
     this.socket.onopen = () => {
       console.log('Browser client connected');
     };
-    console.log("componentDidMount <App />");
-    setTimeout(() => {
-      console.log("Simulating incoming message");
-      // Add a new message to the list of messages in the data store
-      const newMessage = {id: 3, username: "Michelle", content: "Hello there!"};
-      const messages = this.state.messages.concat(newMessage)
-      // Update the state of the app component.
-      // Calling setState will trigger a call to render() in App and all child components.
-      this.setState({messages: messages})
-    }, 3000);
+
+    this.socket.onmessage = (event) => {
+      let newstate = this.state.messages;
+      newstate.push(JSON.parse(event.data))
+      console.log(event.data);
+      this.setState({ messages: newstate })
+    }
   }
 
 
   changeText = (text) => {
     const newText = {
-      username :this.state.currentUser.name,
-      content : text,
-      id: this.state.messages.length +1
+      username: this.state.currentUser.name,
+      content: text,
+      id: this.state.messages.length + 1
     };
-  
-  const newTextState = this.state.messages.push(newText);
-  this.socket.send(JSON.stringify(newText));
+    this.socket.send(JSON.stringify(newText));
 
-    this.setState({ newTextState });
   }
 
 
   changeStateName = (info) => {
-    this.setState({currentUser: {name: info}})
+    this.setState({ currentUser: { name: info } })
   }
 
 
@@ -67,8 +50,8 @@ class App extends Component {
       <div><nav className="navbar">
         <a href="/" className="navbar-brand">Chatty</a>
       </nav>
-      <ChatBar currentUser ={this.state.currentUser.name} changeStateName = {this.changeStateName} changeText ={this.changeText}/>
-      <MessageList messages = {this.state.messages}/>
+        <ChatBar currentUser={this.state.currentUser.name} changeStateName={this.changeStateName} changeText={this.changeText} />
+        <MessageList messages={this.state.messages} />
       </div>
     );
   }
